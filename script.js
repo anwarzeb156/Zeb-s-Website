3f8bfd14104fdd1d4adb666593843cd20b1517cd
Anwar Zeb <136989321+anwarzeb156@users.noreply.github.com>
2024-07-21 21:55:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank You!Form submitted! We will get back to you soon.');
+});
